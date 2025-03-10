3517d91ab0823870e7b69e1041f03be54df54f49
Vinicios Neves <vinicios.neves@mac>
2025-03-10 20:55:43 +0000

    video 1.4

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,47 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+
+import { Image, StyleSheet, Text, View } from "react-native"
+import { FokusButton } from "../components/FokusButton"
 
 export default function Index() {
+
+
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-            <Link href={{ pathname: '/pomodoro' }}>
-                Quero iniciar!
-            </Link>
+        <View
+            style={styles.container}
+        >
+            <Image source={require('../assets/images/logo.png')} />
+            <View style={styles.innerContainer}>
+                <Text style={styles.text}>
+                    Otimize sua{"\n"} produtividade,{"\n"}
+                    <Text style={styles.bold}>
+                        mergulhe no que{"\n"} importa
+                    </Text>
+                </Text>
+                <Image source={require('../assets/images/home.png')} />
+                <FokusButton title="Quero iniciar!" onPress={() => console.log('navegar')} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#021123',
+        gap: 40
+    },
+    innerContainer: {
+        gap: 12
+    },
+    text: {
+        fontSize: 26,
+        lineHeight: 39,
+        textAlign: 'center',
+        color: '#FFF'
+    },
+    bold: {
+        fontWeight: 'bold'
+    }
+})
