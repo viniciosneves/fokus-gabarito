282fd4c11b195a518710d7b22446460a90ed2993
Vinicios Neves <vinicios.neves@mac>
2025-03-10 20:53:58 +0000

    video 1.3

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,7 @@
 import { Stack } from 'expo-router/stack';
 
 export default function Layout() {
-  return <Stack />;
+  return (<Stack>
+        <Stack.Screen name='pomodoro' options={{ headerShown: false }} />
+    </Stack>);
 }
--- app/pomodoro.jsx
+++ app/pomodoro.jsx
@@ -9,19 +9,19 @@ const pomodoro = [
   {
     id: 'focus',
     initialValue: 25 * 60,
-    image: require('./pomodoro.png'),
+    image: require('../assets/images/pomodoro.png'),
     display: 'Foco'
   },
   {
     id: 'short',
     initialValue: 5 * 60,
-    image: require('./short.png'),
+    image: require('../assets/images/short.png'),
     display: 'Pausa curta'
   },
   {
     id: 'long',
     initialValue: 15 * 60,
-    image: require('./long.png'),
+    image: require('../assets/images/long.png'),
     display: 'Pausa longa'
   },
 ]
